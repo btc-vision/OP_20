4419303a5b0eec0209c5dfbb03d9e9030c745768
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-01 00:11:00 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,19 @@ import { MyToken } from './contracts/MyToken';
 
 export function defineSelectors(): void {
     /** OP_NET */
-    ABIRegistry.defineGetterSelector('address', false);
-    ABIRegistry.defineGetterSelector('owner', false);
-    ABIRegistry.defineMethodSelector('isAddressOwner', false);
+    ABIRegistry.defineMethodSelector('isAddressOwner');
 
     /** OP_20 */
-    ABIRegistry.defineMethodSelector('allowance', false);
-    ABIRegistry.defineMethodSelector('approve', true);
-    ABIRegistry.defineMethodSelector('balanceOf', false);
-    ABIRegistry.defineMethodSelector('burn', true);
-    ABIRegistry.defineMethodSelector('mint', true);
-    ABIRegistry.defineMethodSelector('transfer', true);
-    ABIRegistry.defineMethodSelector('transferFrom', true);
-
-    ABIRegistry.defineGetterSelector('decimals', false);
-    ABIRegistry.defineGetterSelector('name', false);
-    ABIRegistry.defineGetterSelector('symbol', false);
-    ABIRegistry.defineGetterSelector('totalSupply', false);
-    ABIRegistry.defineGetterSelector('maximumSupply', false);
+    ABIRegistry.defineMethodSelector('allowance');
+    ABIRegistry.defineMethodSelector('approve');
+    ABIRegistry.defineMethodSelector('balanceOf');
+    ABIRegistry.defineMethodSelector('burn');
+    ABIRegistry.defineMethodSelector('mint');
+    ABIRegistry.defineMethodSelector('transfer');
+    ABIRegistry.defineMethodSelector('transferFrom');
 
     /** Optional */
-    ABIRegistry.defineMethodSelector('airdrop', true);
+    ABIRegistry.defineMethodSelector('airdrop');
 
     // Define your selectors here.
 }
@@ -37,7 +29,7 @@ Blockchain.contract = () => {
     // DO NOT ADD CUSTOM LOGIC HERE.
 
     return contract;
-}
+};
 
 // VERY IMPORTANT
 export * from '@btc-vision/btc-runtime/runtime/exports';
