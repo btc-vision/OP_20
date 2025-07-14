d70c6c8fe0a816c6f6a336bb9ffe70245bb01a6b
gaizenberg888 <100959472+gaizenberg888@users.noreply.github.com>
2025-07-14 22:06:07 +0300

    Update MyToken.ts
    zakkonik

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -21,10 +21,10 @@ export class MyToken extends DeployableOP_20 {
 
     // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
     public override onDeployment(_calldata: Calldata): void {
-        const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
-        const decimals: u8 = 18; // Your decimals.
-        const name: string = 'Test'; // Your token name.
-        const symbol: string = 'TEST'; // Your token symbol.
+        const maxSupply: u256 = u128.fromString('100000000000000000000'); // 1e6 × 10^8
+        const decimals: u8 = 8;
+        const name: string = 'zakonik';
+        const symbol: string = 'ZKN';
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
