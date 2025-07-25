285d61224194e221a2175ca772207e6694995877
Kartyginakaterina <Kartyginakaterina@mail.ru>
2025-07-25 22:43:03 +0300

    Update MyToken.ts

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -21,10 +21,10 @@ export class MyToken extends DeployableOP_20 {
 
     // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
     public override onDeployment(_calldata: Calldata): void {
-        const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
+        const maxSupply: u256 = u256.fromString('1000000000'); // Your max supply. (Here, 1 billion tokens)
         const decimals: u8 = 18; // Your decimals.
-        const name: string = 'Test'; // Your token name.
-        const symbol: string = 'TEST'; // Your token symbol.
+        const name: string = 'katubuhi'; // Your token name.
+        const symbol: string = 'KKKKK'; // Your token symbol.
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
@@ -102,6 +102,6 @@ export class MyToken extends DeployableOP_20 {
         const writer: BytesWriter = new BytesWriter(BOOLEAN_BYTE_LENGTH);
         writer.writeBoolean(true);
 
-        return writer;
+        npm run build;
     }
 }
